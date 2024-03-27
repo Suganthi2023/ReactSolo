021f64e4baf0eba6300ee9adeb8b4128aa276096
Suganthi2023 <suganthir2000@gmail.com>
2024-03-27 01:07:48 -0500

    Initial Set up of the the filter form

--- reactsolo/src/components/App.js
+++ reactsolo/src/components/App.js
@@ -3,6 +3,7 @@ import PackageList from "./PackageList";
 import { useState,useEffect } from 'react';
 import axios from "axios";
 import config from "../services/config.json"
+import PackageFilter from './PackageFilter';
 function App() {
 
   const [packagelists,setPackagelists]=useState([]);
@@ -11,12 +12,33 @@ function App() {
     getLists();
   },[])
 
-  
-
   const getLists =async () => {
     const response = await axios.get(`https://${config.apikey}.mockapi.io/PackageCard`)
     setPackagelists(response.data);
   }
+
+  const [query,setQuery]=useState("");
+  const [selectType,setSelectType]=useState("Destination");
+  const [minPrice,setMinPrice]=useState('');
+  const [maxPrice,setMaxPrice]=useState('');
+  const [minRating,setMinRating]=useState('');
+  const [maxRating,setMaxRating]=useState('');
+  const [minDuration,setMinDuration]=useState('');
+  const [maxDuration,setMaxDuration]=useState('');
+
+  /*const handleInputText=(e)=>{
+    e.preventDefault();
+    const userText=e.target.value;
+    setQuery(userText);
+  }*/
+
+  const changeHandle =(e)=>{
+    e.preventDefault();
+    console.log("The selection option is ",e.target.value);
+
+  }
+
+  
 
   return (
 
@@ -24,6 +46,8 @@ function App() {
       <div className="App">
       <h2>Travel Package Browsing Plateform</h2>
       <PackageList lists={packagelists} />
+      <PackageFilter textinput={query}  typeSelect={selectType} miniPrice={minPrice} maxiPrice={maxPrice} 
+      miniRating={minRating} maxiRating={maxRating} miniDuration={minDuration} maxiDuration={maxDuration} handleChange={changeHandle}/>
       </div>
     </>
   );
--- reactsolo/src/components/PackageFilter.js
+++ reactsolo/src/components/PackageFilter.js
@@ -1,10 +1,44 @@
 
-function PackageFilter(){
+function PackageFilter({textinput,typeSelect,miniPrice,maxiPrice,miniRating,maxiRating,
+                        miniDuration,maxiDuration,handleChange}){
+    
 
     return(
         <>
             <div>
-                <input type="text"/>
+                <form>
+                    <input type="text" name="name" value={textinput} onChange={handleChange}/>
+                    <select id="selecttype" value={typeSelect} onChange={handleChange}>
+                        <option value="Destination">Destination</option>
+                        <option value="Rating">Rating</option>
+                        <option value="Duration">Duration</option>
+                        <option value="Price">Price</option>
+                    </select>
+                    {typeSelect === "Price"&&(
+                        <>
+                            <label>Min Price:</label>
+                            <input type="number" value={miniPrice} onChange={handleChange}/>
+                            <label>Max Price:</label>
+                            <input type="number" value={maxiPrice} onChange={handleChange}/>
+                        </>
+                    )}
+                    {typeSelect === "Rating"&&(
+                        <>
+                            <label>Min Price:</label>
+                            <input type="number" value={miniRating} onChange={handleChange}/>
+                            <label>Max Price:</label>
+                            <input type="number" value={maxiRating} onChange={handleChange}/>
+                        </>
+                    )}
+                    {typeSelect === "Duration"&&(
+                        <>
+                            <label>Min Price:</label>
+                            <input type="number" value={miniDuration} onChange={handleChange}/>
+                            <label>Max Price:</label>
+                            <input type="number" value={maxiDuration} onChange={handleChange}/>
+                        </>
+                    )}
+                </form>
             </div>
 
         </>
